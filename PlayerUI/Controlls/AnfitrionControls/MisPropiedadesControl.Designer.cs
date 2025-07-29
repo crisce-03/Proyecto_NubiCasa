e41dbf6ddadf6a02d65a68b7d5d27c760154613b
Axel Cerritos <axelenriquecerritos@gmail.com>
2025-07-28 23:32:02 -0600

    Diseño de propiedad con cards y boton eliminar

--- PlayerUI/Controlls/AnfitrionControls/MisPropiedadesControl.Designer.cs
+++ PlayerUI/Controlls/AnfitrionControls/MisPropiedadesControl.Designer.cs
@@ -41,17 +41,19 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1013, 151);
+            this.panel1.Size = new System.Drawing.Size(1140, 189);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(42)))), ((int)(((byte)(83)))));
             this.button1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(43, 61);
+            this.button1.Location = new System.Drawing.Point(48, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(278, 67);
+            this.button1.Size = new System.Drawing.Size(313, 84);
             this.button1.TabIndex = 1;
             this.button1.Text = "Agregar Propiedad  +";
             this.button1.UseVisualStyleBackColor = false;
@@ -62,27 +64,31 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(356, 9);
+            this.label1.Location = new System.Drawing.Point(400, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(274, 45);
+            this.label1.Size = new System.Drawing.Size(332, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mis Propiedades";
             // 
             // flowLayoutPanelPropiedades
             // 
+            this.flowLayoutPanelPropiedades.AutoScroll = true;
+            this.flowLayoutPanelPropiedades.BackColor = System.Drawing.Color.Transparent;
             this.flowLayoutPanelPropiedades.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanelPropiedades.Location = new System.Drawing.Point(0, 151);
+            this.flowLayoutPanelPropiedades.Location = new System.Drawing.Point(0, 189);
+            this.flowLayoutPanelPropiedades.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.flowLayoutPanelPropiedades.Name = "flowLayoutPanelPropiedades";
-            this.flowLayoutPanelPropiedades.Size = new System.Drawing.Size(1013, 489);
+            this.flowLayoutPanelPropiedades.Size = new System.Drawing.Size(1140, 611);
             this.flowLayoutPanelPropiedades.TabIndex = 1;
             // 
             // MisPropiedadesControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1013, 640);
+            this.ClientSize = new System.Drawing.Size(1140, 800);
             this.Controls.Add(this.flowLayoutPanelPropiedades);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "MisPropiedadesControl";
             this.Text = "MisPropiedadesControlcs";
             this.Load += new System.EventHandler(this.MisPropiedadesForm_Load);
--- PlayerUI/Controlls/AnfitrionControls/MisPropiedadesControl.cs
+++ PlayerUI/Controlls/AnfitrionControls/MisPropiedadesControl.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using PlayerUI.Forms;
 
@@ -21,12 +16,11 @@ namespace PlayerUI.Controlls.AnfitrionControls
         {
             InitializeComponent();
             idAnfitrion = ID_Anfitrion;
-    
         }
 
         private void btnAgregarPropiedad_Click(object sender, EventArgs e)
         {
-            AgregarPropiedadForm agregarPropiedad =new  AgregarPropiedadForm(idAnfitrion);
+            AgregarPropiedadForm agregarPropiedad = new AgregarPropiedadForm(idAnfitrion);
             agregarPropiedad.Show();
         }
 
@@ -35,6 +29,66 @@ namespace PlayerUI.Controlls.AnfitrionControls
             CargarPropiedades();
         }
 
+        // Botón con bordes redondeados y efecto hover
+        private class RoundedButton : Button
+        {
+            private int borderRadius = 15;
+            private Color normalColor;
+            private Color hoverColor;
+
+            public RoundedButton(string text, Color backColor, Color hoverBackColor)
+            {
+                this.Text = text;
+                this.normalColor = backColor;
+                this.hoverColor = hoverBackColor;
+                this.BackColor = normalColor;
+                this.ForeColor = Color.White;
+                this.FlatStyle = FlatStyle.Flat;
+                this.FlatAppearance.BorderSize = 0;
+                this.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                this.Width = 90;
+                this.Height = 30;
+                this.Margin = new Padding(10, 0, 0, 0);
+                this.DoubleBuffered = true;
+
+                this.MouseEnter += (s, e) => this.BackColor = hoverColor;
+                this.MouseLeave += (s, e) => this.BackColor = normalColor;
+            }
+
+            protected override void OnPaint(PaintEventArgs pevent)
+            {
+                pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                Rectangle rect = this.ClientRectangle;
+                using (GraphicsPath path = GetRoundedRectanglePath(rect, borderRadius))
+                {
+                    this.Region = new Region(path);
+                    using (SolidBrush brush = new SolidBrush(this.BackColor))
+                    {
+                        pevent.Graphics.FillPath(brush, path);
+                    }
+                    TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font, rect, this.ForeColor,
+                        TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+                }
+            }
+
+            private GraphicsPath GetRoundedRectanglePath(Rectangle rect, int radius)
+            {
+                GraphicsPath path = new GraphicsPath();
+                int diameter = radius * 2;
+                path.StartFigure();
+                path.AddArc(rect.Left, rect.Top, diameter, diameter, 180, 90);
+                path.AddArc(rect.Right - diameter, rect.Top, diameter, diameter, 270, 90);
+                path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+                path.AddArc(rect.Left, rect.Bottom - diameter, diameter, diameter, 90, 90);
+                path.CloseFigure();
+                return path;
+            }
+        }
+
+        private RoundedButton CrearBoton(string texto, Color colorFondo, Color colorHover)
+        {
+            return new RoundedButton(texto, colorFondo, colorHover);
+        }
 
         private void CargarPropiedades()
         {
@@ -43,88 +97,204 @@ namespace PlayerUI.Controlls.AnfitrionControls
             using (SqlConnection con = Conexion.ObtenerConexion())
             {
                 SqlCommand cmd = new SqlCommand(@"
-            SELECT Id_Propiedad, Nombre, Precio, RutaImagen 
-            FROM Propiedades 
-            WHERE IdAnfitrion = @id AND Activo = 1", con); // muestra solo activos
+                    SELECT Id_Propiedad, Nombre, Precio, RutaImagen 
+                    FROM Propiedades 
+                    WHERE IdAnfitrion = @id AND Activo = 1", con);
 
                 cmd.Parameters.AddWithValue("@id", idAnfitrion);
-
                 SqlDataReader reader = cmd.ExecuteReader();
 
                 while (reader.Read())
                 {
-                    // Crear tarjeta (panel)
-                    Panel card = new Panel();
-                    card.Width = 200;
-                    card.Height = 250;
-                    card.Margin = new Padding(10);
-                    card.BorderStyle = BorderStyle.FixedSingle;
+                    int idPropiedad = Convert.ToInt32(reader["Id_Propiedad"]);
+
+                    // Panel principal con bordes redondeados
+                    RoundedPanel card = new RoundedPanel
+                    {
+                        Width = 240,
+                        Height = 360,
+                        Margin = new Padding(10),
+                        BorderRadius = 20,
+                        BackColor = Color.White
+                    };
+
+                    // Layout principal
+                    TableLayoutPanel layout = new TableLayoutPanel
+                    {
+                        Dock = DockStyle.Fill,
+                        RowCount = 5,
+                        ColumnCount = 1,
+                        BackColor = Color.Transparent, // TRANSPARENTE para eliminar borde rectangular
+                        Padding = new Padding(5)
+                    };
+
+                    layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 100)); // Imagen
+                    layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));  // Info
+                    layout.RowStyles.Add(new RowStyle(SizeType.Percent, 100));  // Espacio
+                    layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 45));  // Botones 1
+                    layout.RowStyles.Add(new RowStyle(SizeType.Absolute, 45));  // Botones 2
 
                     // Imagen
-                    PictureBox pic = new PictureBox();
-                    pic.Width = 180;
-                    pic.Height = 100;
-                    pic.SizeMode = PictureBoxSizeMode.StretchImage;
-                    pic.Top = 10;
-                    pic.Left = 10;
+                    PictureBox pic = new PictureBox
+                    {
+                        Dock = DockStyle.Fill,
+                        SizeMode = PictureBoxSizeMode.Zoom,
+                        Margin = new Padding(0),
+                        BackColor = Color.Transparent // TRANSPARENTE para quitar bordes feos
+                    };
 
                     string rutaImagen = reader["RutaImagen"].ToString();
-                    if (File.Exists(rutaImagen))
+                    pic.Image = File.Exists(rutaImagen)
+                        ? Image.FromFile(rutaImagen)
+                        : Properties.Resources.imagen_no_disponible;
+
+                    // Info
+                    Panel infoPanel = new Panel { Dock = DockStyle.Fill, BackColor = Color.Transparent };
+
+                    Label lblNombre = new Label
                     {
-                        pic.Image = Image.FromFile(rutaImagen);
-                    }
-                    else
+                        Text = reader["Nombre"].ToString(),
+                        Font = new Font("Segoe UI", 11, FontStyle.Bold),
+                        ForeColor = Color.Black,
+                        Dock = DockStyle.Top,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        BackColor = Color.Transparent
+                    };
+
+                    Label lblEstado = new Label
                     {
-                        // Imagen por defecto si no existe la ruta
-                        pic.Image = Properties.Resources.imagen_no_disponible; // agrega esta imagen a tus recursos si deseas
-                    }
+                        Text = "Disponible",
+                        Font = new Font("Segoe UI", 9, FontStyle.Regular),
+                        ForeColor = Color.Green,
+                        Dock = DockStyle.Top,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Margin = new Padding(0, 5, 0, 0),
+                        BackColor = Color.Transparent
+                    };
 
-                    // Nombre
-                    Label lblNombre = new Label();
-                    lblNombre.Text = reader["Nombre"].ToString();
-                    lblNombre.Top = 120;
-                    lblNombre.Left = 10;
-                    lblNombre.Width = 180;
+                    infoPanel.Controls.Add(lblEstado);
+                    infoPanel.Controls.Add(lblNombre);
 
-                    // Precio
-                    Label lblPrecio = new Label();
-                    lblPrecio.Text = $"${reader["Precio"]} / noche";
-                    lblPrecio.Top = 145;
-                    lblPrecio.Left = 10;
-                    lblPrecio.Width = 180;
+                    // Botones 1 (Editar, Ocultar)
+                    FlowLayoutPanel botones1 = new FlowLayoutPanel
+                    {
+                        Dock = DockStyle.Fill,
+                        FlowDirection = FlowDirection.LeftToRight,
+                        WrapContents = false,
+                        BackColor = Color.Transparent,
+                        Padding = new Padding(0, 10, 0, 0),
+                        AutoSize = false
+                    };
 
-                    // Botón Editar
-                    Button btnEditar = new Button();
-                    btnEditar.Text = "Editar";
-                    btnEditar.Top = 180;
-                    btnEditar.Left = 10;
-                    btnEditar.Width = 80;
-                    // opcional: agregar evento click para editar
+                    Button btnEditar = CrearBoton("Editar", Color.DodgerBlue, Color.RoyalBlue);
+                    Button btnOcultar = CrearBoton("Ocultar", Color.Orange, Color.DarkOrange);
 
-                    // Botón Ocultar
-                    Button btnOcultar = new Button();
-                    btnOcultar.Text = "Ocultar";
-                    btnOcultar.Top = 180;
-                    btnOcultar.Left = 100;
-                    btnOcultar.Width = 80;
-                    // opcional: evento click para desactivar la propiedad
+                    botones1.Controls.Add(btnEditar);
+                    botones1.Controls.Add(btnOcultar);
 
-                    // Agregar controles al panel
-                    card.Controls.Add(pic);
-                    card.Controls.Add(lblNombre);
-                    card.Controls.Add(lblPrecio);
-                    card.Controls.Add(btnEditar);
-                    card.Controls.Add(btnOcultar);
+                    // Botones 2 (Visualizar, Eliminar)
+                    FlowLayoutPanel botones2 = new FlowLayoutPanel
+                    {
+                        Dock = DockStyle.Fill,
+                        FlowDirection = FlowDirection.LeftToRight,
+                        WrapContents = false,
+                        BackColor = Color.Transparent,
+                        Padding = new Padding(0, 5, 0, 10),
+                        AutoSize = false
+                    };
 
-                    // Agregar el panel al FlowLayout
+                    Button btnVisualizar = CrearBoton("Visualizar", Color.SeaGreen, Color.MediumSeaGreen);
+                    Button btnEliminar = CrearBoton("Eliminar", Color.Crimson, Color.DarkRed);
+
+                    // Funcionalidad eliminar (solo UI)
+                    btnEliminar.Click += (s, e) =>
+                    {
+                        if (MessageBox.Show("¿Seguro que quieres eliminar esta propiedad?", "Eliminar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                        {
+                            try
+                            {
+                                using (SqlConnection conexion = Conexion.ObtenerConexion())
+                                {
+                                    SqlCommand cmdEliminar = new SqlCommand("DELETE FROM Propiedades WHERE Id_Propiedad = @id", conexion);
+                                    cmdEliminar.Parameters.AddWithValue("@id", idPropiedad);
+                                    cmdEliminar.ExecuteNonQuery();
+                                }
+
+                                flowLayoutPanelPropiedades.Controls.Remove(card);
+                                card.Dispose();
+
+                                MessageBox.Show("Propiedad eliminada correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Error al eliminar la propiedad: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+                        }
+                    };
+
+                    botones2.Controls.Add(btnVisualizar);
+                    botones2.Controls.Add(btnEliminar);
+
+                    // Agregar controles al layout
+                    layout.Controls.Add(pic, 0, 0);
+                    layout.Controls.Add(infoPanel, 0, 1);
+                    layout.Controls.Add(new Panel { BackColor = Color.Transparent }, 0, 2); // espacio vacío transparente
+                    layout.Controls.Add(botones1, 0, 3);
+                    layout.Controls.Add(botones2, 0, 4);
+
+                    // Agregar layout al panel redondeado
+                    card.Controls.Add(layout);
+
+                    // Finalmente agregar la card al flowLayoutPanel
                     flowLayoutPanelPropiedades.Controls.Add(card);
                 }
 
                 reader.Close();
             }
         }
-
-
     }
 
+    // Panel con bordes redondeados
+    public class RoundedPanel : Panel
+    {
+        public int BorderRadius { get; set; } = 15;
+
+        public RoundedPanel()
+        {
+            this.DoubleBuffered = true;
+            this.BackColor = Color.White;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle bounds = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+            using (var path = GetRoundedRectanglePath(bounds, BorderRadius))
+            using (var brush = new SolidBrush(this.BackColor))
+            using (var pen = new Pen(Color.LightGray, 1))
+            {
+                e.Graphics.FillPath(brush, path);
+                e.Graphics.DrawPath(pen, path);
+            }
+        }
+
+        private GraphicsPath GetRoundedRectanglePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int diameter = radius * 2;
+
+            path.StartFigure();
+            path.AddArc(rect.Left, rect.Top, diameter, diameter, 180, 90);
+            path.AddArc(rect.Right - diameter, rect.Top, diameter, diameter, 270, 90);
+            path.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            path.AddArc(rect.Left, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+    }
 }
+
+
